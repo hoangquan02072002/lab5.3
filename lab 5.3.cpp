6da7685831fe7727af61c22b33d672acd7a65f9c
nguyen le hoang quan <hoangquan02072002@gmail.com>
2022-01-18 02:58:00 +0300

    first commit

---
+++ lab 5.3.cpp
@@ -0,0 +1,43 @@
+#include "matrix.h"
+
+using namespace std;
+int main() {
+	
+	Matr3Diag matrA;
+	matrA.input(3);
+	Matr3Diag matrB;
+	cin >> matrB;
+
+	cout << "matrA:" << endl;
+	cout << matrA << endl;
+
+	cout << "matrB:" << endl;
+	cout << matrB << endl;
+
+	cout << "matrA += matrB" << endl;
+	matrA += matrB;
+	cout << matrA << endl;
+
+	cout << "matrA -= matrB" << endl;
+	matrA -= matrB;
+	cout << matrA << endl;
+
+	cout << "matrA += matrB" << endl;
+	matrA += matrB;
+	cout << matrA << endl;
+
+	cout << "matrA + matrB" << endl;
+	cout << matrA + matrB << endl;
+	cout << "matrA - matrB" << endl;
+	cout << matrA - matrB << endl;
+	cout << "-matrA" << endl;
+	cout << (-matrA) << endl;
+	cout << "matrA * matrB" << endl;
+	cout << matrA * matrB << endl;
+	cout << "matrA * 5" << endl;
+	cout << matrA * 5 << endl;
+	cout << "matrB det()" << endl;
+	cout << matrB.det() << endl;
+	cout << "matrB trace()" << endl;
+	cout << matrB.trace() << endl;
+}
